ab9b5c4d2d8ad5ce523879762a6dfb6423daef92
BuildTools <unconfigured@null.spigotmc.org>
2018-08-28 14:17:41 +0200

    my approach to compound primary keys

--- SortableAction.php
+++ SortableAction.php
@@ -9,21 +9,20 @@ use yii\base\InvalidConfigException;
 class SortableAction extends Action {
     /**
      * (required) The ActiveRecord Class name
-     * 
+     *
      * @var string
      */
     public $activeRecordClassName;
-    
+
     /**
      * (required) The attribute name where your store the sort order.
-     * 
+     *
      * @var string
      */
     public $orderColumn;
-    
+
     public function init(){
         parent::init();
-        
         if(!isset($this->activeRecordClassName)){
             throw new InvalidConfigException("You must specify the activeRecordClassName");
         }
@@ -32,16 +31,15 @@ class SortableAction extends Action {
             throw new InvalidConfigException("You must specify the orderColumn");
         }
     }
-    
     public function run(){
         if(!\Yii::$app->request->isAjax){
             throw new HttpException(404);
         }
-        
         if (isset($_POST['items']) && is_array($_POST['items'])) {
             $activeRecordClassName = $this->activeRecordClassName;
             foreach ($_POST['items'] as $i=>$item) {
                 $page = $activeRecordClassName::findOne($item);
+                //$page = $activeRecordClassName::find()->where($item);
                 $page->updateAttributes([
                     $this->orderColumn => $i,
                 ]);
--- assets/js/sortable-grid-view.js
+++ assets/js/sortable-grid-view.js
@@ -8,39 +8,51 @@
 	    csrfTokenName: '',
 	    csrfToken: '',
 	};
-	
+
 	$.extend({}, defaultOptions, options);
-	
+
 	$('body').append('<div class="modal fade" id="' + options.id + '-sorting-modal" tabindex="-1" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-body">' + options.sortingPromptText + '</div></div></div></div>');
-	
+
 	var regex = /items\[\]\_(\d+)/;
-	
+
 	$('#' + options.id + ' .sortable-grid-view tbody').sortable({
 	    update : function () {
 		$('#' + options.id + '-sorting-modal').modal('show');
-		serial = $('#' + options.id + ' .sortable-grid-view tbody').sortable('toArray', {attribute:'id'});
+		//serial = $('#' + options.id + ' .sortable-grid-view tbody').sortable('toArray', {attribute:'id'});
+        serial = [];
+
+
+        $('#' + options.id + ' .sortable-grid-view tbody .ui-sortable-handle').each( function() {
+            //console.log($(this).data('key'));
+            serial.push($(this).data('key'));
+        });
+
+        //console.log(serial);
+
 
 		var length = serial.length;
 		var currentRecordNo = 0;
 		var successRecordNo = 0;
-		var data = '';
-		
+		var data = [];
+
 		if(length > 0){
 		    for(var i=0; i<length; i++){
-			var itemID = regex.exec(serial[i]);
-			data = data + 'items[' + i + ']=' + itemID[1] + '&';
+			//var itemID = regex.exec(serial[i]);
+            var itemID = serial[i];
+			//data = data + 'items[' + i + ']=' + itemID[1] + '&';
+            data.push(itemID)
 			currentRecordNo++;
-			
+
 			if(currentRecordNo == 500 || i == (length-1)){
-			    data =  data + options.csrfTokenName + '=' + options.csrfToken;
-			    
+			    //data =  data + options.csrfTokenName + '=' + options.csrfToken;
+
 			    (function(currentRecordNo){
 				$.ajax({
 				    'url': options.action,
 				    'type': 'post',
-				    'data': data,
+				    'data': { 'items': data, [options.csrfTokenName] : options.csrfToken },
 				    success: function(data){
-					checkSuccess(currentRecordNo);
+					                   checkSuccess(currentRecordNo);
 				    },
 				    error: function(data){
 					$('#' + options.id + '-sorting-modal').modal('hide');
@@ -48,16 +60,16 @@
 				    }
 				});
 			    })(currentRecordNo);
-			    
+
 			    currentRecordNo = 0;
-			    data = '';
+			    data = [];
 			}
 		    }
 		}
-		
+
 		function checkSuccess(count){
 		    successRecordNo += count;
-		    
+
 		    if(successRecordNo >= length){
 			$('#' + options.id + '-sorting-modal').modal('hide');
 		    }
